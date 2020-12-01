e4b8fd9ddc45f054f4fb19bb89219045d17a57a2
ParthJoshi009 <74600742+ParthJoshi009@users.noreply.github.com>
2020-12-01 22:55:59 +0530

    Added files via upload

---
+++ SourceControlAssignment1/MVCValidations/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCValidations.Models;
+
+namespace MVCValidations.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SubmitData(Student stu)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.stu = stu;
+                return View("SubmitData");
+            }
+            return View("Index");
+        }
+    }
+}
