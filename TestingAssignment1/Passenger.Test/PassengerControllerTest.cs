e1b3924e9ceed848f48a53ca05ab40fe517a9a8e
ParthJoshi009 <74600742+ParthJoshi009@users.noreply.github.com>
2021-03-03 00:01:02 +0530

    Passenget Test Added

---
+++ TestingAssignment1/Passenger.Test/PassengerControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace Passenger.Test
+{
+    public class PassengerControllerTest
+    {
+        
+        [Fact]
+        public void Test_GetPassenger()
+        {
+            
+        }
+    }
+}
---
+++ TestingAssignment1/Passenger.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Passenger.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Passenger.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60e3df56-70c5-455b-acbe-b31777844f7f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
