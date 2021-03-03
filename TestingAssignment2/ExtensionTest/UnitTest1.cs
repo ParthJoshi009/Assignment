8359a3a3f60735c80a838b0a7dd1057d74334170
ParthJoshi009 <74600742+ParthJoshi009@users.noreply.github.com>
2021-03-03 23:24:38 +0530

    Update UnitTest1.cs

--- TestingAssignment2/ExtensionTest/UnitTest1.cs
+++ TestingAssignment2/ExtensionTest/UnitTest1.cs
@@ -8,9 +8,95 @@ namespace ExtensionTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void Test_ConvertLowerCasePass()
         {
+            //Arrange
+            string str = "Hello";
+            //Act
+            string result = str.ConvertLowerCase();
+            //Assert
+            Assert.Equal("Success", result);
         }
+
+        [Fact]
+        public void Test_ConvertLowerCaseFail()
+        {
+            //Arrange
+            string str = "hello";
+            //Act
+            string result = str.ConvertLowerCase();
+            //Assert
+            Assert.NotEqual("Not Success", result);
+        }
+
+      
+        [Fact]
+        public void Test_ConvertLowerCasePass()
+        {
+            //Arrange
+            string str = "HELLo";
+            //Act
+            string result = str.ConvertLowerCase();
+            //Assert
+            Assert.Equal("hello", result);
+        }
+
+        //Return a string of Uppercase to Lowercase TEST FAIL.
+        [Fact]
+        public void Test_ConvertLowerCaseFail()
+        {
+            //Arrange
+            string str = "HELLo";
+            //Act
+            string result = str.ConvertToLowerCase();
+            //Assert
+            Assert.NotEqual("Hello", result);
+        }
+
+        [Fact]
+        public void Test_ConvertTitleCasePass()
+        {
+            //Arrange
+            string str = "hello";
+            //Act
+            string result = str.ConvertTitleCase();
+            //Assert
+            Assert.Equal("Hello", result);
+        }
+
+        [Fact]
+        public void Test_ConvertTitleCaseFail()
+        {
+            //Arrange
+            string str = "HELLO";
+            //Act
+            string result = str.ConvertTitleCase();
+            //Assert
+            Assert.NotEqual("hellO", result);
+        }
+
+        [Fact]
+        public void Test_AllCharLowerCasePass()
+        {
+            //Arrange
+            string str = "hello";
+            //Act
+            bool result = str.AllCharLowerCase();
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test_AllCharLowerCaseFail()
+        {
+            //Arrange
+            string str = "Hello";
+            //Act
+            bool result = str.AllCharLowerCase();
+            //Assert
+            Assert.False(result);
+        }
+
     }
 }
